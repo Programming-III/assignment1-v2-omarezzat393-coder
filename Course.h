1b96f8d236685a71dc11d9449e2b75347125084a
Omar Mahfouz 16001798 <omarezzat393@gmail.com>
2025-11-11 09:49:10 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,39 @@
+
 #ifndef COURSE_H
 #define COURSE_H
 
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+class Course {
+private:
+    string coursecode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudent;
+public:
+    Course(string coursecode,string courseName,int maxStudents,Student *students,int currentStudents ) {
+        this-> coursecode=coursecode;
+        this-> courseName=courseName;
+        this-> maxStudents=maxStudents;
+        this-> students=students;
+        this->currentStudent=currentStudent;
+    }
+    ~Course() {
+        delete students;
+    }
+    void addStudent(const Student& s) {
+        if (currentStudent< maxStudents) {
+            students[currentStudent]=s;
+        }
+        currentStudent++;
+    }
+    void displayCourseInfo() {
+        cout<<"Course:" << coursecode << "-" << courseName << endl;
+        cout<<"max students:"<<maxStudents<<endl;
+        cout<< "";
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 #endif
