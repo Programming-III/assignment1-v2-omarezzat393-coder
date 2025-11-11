6e21aa1e27a534fbf89182fa662d71c013445cf0
Omar Mahfouz 16001798 <omarezzat393@gmail.com>
2025-11-11 09:48:02 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -9,8 +9,17 @@ private:
     string name;
     int id;
 public:
-    Person(string name,int id);
-    void display();
+    Person(string name,int id) {
+        this->name=name;
+        this->id=id;
+    }
+    void display() {
+        cout<<name<<endl;
+        cout<<id<< endl;
+    }
+    string getname() {
+        return name;
+    }
 };
 
 #endif
